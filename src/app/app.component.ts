af04ac1c8bd55957c9bc9fb5e53fccd1e65b6c6f
Jun Kawa <hubukinokaze@live.com>
2017-09-07 03:06:29 -0700

    + added simple animations to each project card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild  } from '@angular/core';
 import {Http, Headers} from "@angular/http";
 import { MdMenu, MdToolbar, MdButton, MdIcon, MdSidenav} from '@angular/material';
-import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
+import { trigger,state,style,transition,animate,keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,7 @@ import { trigger,state,style,transition,animate,keyframes } from '@angular/anima
   animations: [
     trigger('slideInOut', [
       transition('* <=> *', [
-        animate(300, keyframes([
+        animate('1200ms cubic-bezier(.5, 0, .5, 1)', keyframes([
           style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
           style({opacity: 1, transform: 'translateX(-15px)', offset: 0.2}),
           style({opacity: 0, transform: 'translateX(100%)',  offset: 0.3}),
@@ -20,6 +20,19 @@ import { trigger,state,style,transition,animate,keyframes } from '@angular/anima
         ]))
       ])
     ]),
+    trigger('flyInOut', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('* => *', [
+        animate(300, keyframes([
+          style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+          style({opacity: 1, transform: 'translateX(-15px)', offset: 0.2}),
+          style({opacity: 0, transform: 'translateX(100%)',  offset: 0.3}),
+          style({opacity: 0, transform: 'translateX(-100%)', offset: 0.5}),
+          style({opacity: 1, transform: 'translateX(15px)',  offset: 0.8}),
+          style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+        ]))
+      ])
+    ])
   ]
 })
 
@@ -31,11 +44,13 @@ export class AppComponent {
 
   private navButtons: any[];
   private projects: any[];
+  private projectState: any[];
 
   constructor(private http: Http) {
     this.githubApi = 'https://api.github.com/users/hubukinokaze/repos';
     this.switchBool = 'Home';
     this.profileState = '1';
+    this.projectState = [];
     this.navButtons = [
       {
         name: "Home",
@@ -68,11 +83,21 @@ export class AppComponent {
     return (this.http.get(this.githubApi, {headers: headers})).subscribe(data => {
       // Read the result field from the JSON response.
       this.projects = data.json();
+      this.populateProjectState();
     });
+  }
+
+  private populateProjectState() {
+    for(let i = 0; i < this.projects.length; i++) {
+      this.projectState.push(i);
+    }
   }
 
   private animateSlide() {
     this.profileState = (this.profileState === '1' ? '2' : '1');
   }
 
+  private animateProjects(i) {
+    this.projectState[i] = (this.projectState[i] === i ? -i : i);
+  }
 }
