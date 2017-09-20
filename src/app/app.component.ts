99dea08d8b34ca669a30072af88d0cd4065054a9
Jun Kawa <hubukinokaze@live.com>
2017-09-19 23:39:06 -0700

    + added fade in animation + added twril animation + changed orange to sea orange +

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,16 +20,25 @@ import { trigger,state,style,transition,animate,keyframes, query, stagger } from
         ]))
       ])
     ]),
-    trigger('flyInOut', [
-      state('in', style({transform: 'translateX(0)'})),
+    trigger('fadeIn', [
       transition('* => *', [
-        animate(300, keyframes([
-          style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
-          style({opacity: 1, transform: 'translateX(-15px)', offset: 0.2}),
-          style({opacity: 0, transform: 'translateX(100%)',  offset: 0.3}),
-          style({opacity: 0, transform: 'translateX(-100%)', offset: 0.5}),
-          style({opacity: 1, transform: 'translateX(15px)',  offset: 0.8}),
-          style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+        query(':enter', [
+          style({ opacity: 0 }),
+          stagger(100, [
+            animate(500, style({ opacity: 1 }))
+          ])
+        ], { optional: true })
+      ])
+    ]),
+    trigger('twirl', [
+      transition('* <=> *', [
+        animate('1200ms cubic-bezier(.5, 0, .5, 1)', keyframes([
+          style({opacity: 1, transform: 'rotateY(0deg)',     offset: 0}),
+          style({opacity: 1, transform: 'rotateY(360deg)', offset: 0.2}),
+          style({opacity: 1, transform: 'rotateY(0deg)',  offset: 0.3}),
+          style({opacity: 1, transform: 'rotateY(360deg)', offset: 0.5}),
+          style({opacity: 1, transform: 'rotateY(0deg)',  offset: 0.8}),
+          style({opacity: 1, transform: 'rotateY(360deg)',     offset: 1.0})
         ]))
       ])
     ])
@@ -76,6 +85,8 @@ export class AppComponent {
   }
 
   private getYourRepos() {
+    this.projects = [];
+
     // create headers
     let headers = new Headers({'Content-Type': 'application/json'});
 
@@ -89,7 +100,7 @@ export class AppComponent {
 
   private populateProjectState() {
     for(let i = 0; i < this.projects.length; i++) {
-      this.projectState.push(i);
+      this.projectState.push(1);
     }
   }
 
@@ -98,6 +109,7 @@ export class AppComponent {
   }
 
   private animateProjects(i) {
-    this.projectState[i] = (this.projectState[i] === i ? -i : i);
+    this.projectState[i] = (this.projectState[i] === 1 ? -1 : 1);
+    console.log(i,this.projectState[i]);
   }
 }
